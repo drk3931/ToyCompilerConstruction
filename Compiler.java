a7a7baf43b54326a462909728dd28fd326f02541
unknown <drk3931@gmail.com>
2019-11-07 17:39:23 -0500

    unary minus added

--- Compiler.java
+++ Compiler.java
@@ -37,6 +37,10 @@ public class Compiler {
         }
     }
 
+    public boolean isAddOperation(char s) {
+        return s == '-' || s == '+';
+    }
+
     public boolean isAlpha(char s) {
         return Character.isAlphabetic(s);
     }
@@ -47,7 +51,7 @@ public class Compiler {
 
     private void writeToOutput(String s) {
         if (!errorFlag) {
-            System.out.println(s);
+            System.out.println('\t' + s);
         }
     }
 
@@ -98,11 +102,18 @@ public class Compiler {
     // <expression> ::= <term> [<addop> <term>]*
     public void expression() {
 
+        // to accomplish +3 or -1
+        // it as 0 + 3 or 0 - 1
+        // so simply set D0 to 0 and let the while loop take care of the plus/minus
+        // operation as it would with a
+        // standard expression
+        //
+
         term();
 
         while (look == '+' || look == '-') {
 
-            writeToOutput("MOVE D0, D1");
+            writeToOutput("MOVE D0, -(SP)");
 
             switch (look) {
             case ('+'): {
@@ -126,7 +137,7 @@ public class Compiler {
 
         matchChar('+');
         term();
-        writeToOutput("ADD D1,D0");
+        writeToOutput("ADD (SP)+,D0");
 
     }
 
@@ -145,13 +156,82 @@ public class Compiler {
 
         matchChar('-');
         term();
-        writeToOutput("SUBTRACT D1,D0");
-        System.out.println("NEG D0");
+        writeToOutput("SUB (SP)+,D0");
+        writeToOutput("NEG D0");
 
     }
 
+    // ((1 + 2) + (2 + 3))/(2*2)
+
+    public void factor() {
+
+        if (isAddOperation(look)) {
+
+            //this is adding an implicit 0 in the place of getnumber if you put in something like
+            //-1
+            writeToOutput("CLR D0");
+
+        }
+        else if (look == '(') {
+            matchChar('(');
+            expression();
+            matchChar(')');
+        } else {
+            writeToOutput("MOVE #" + getNumber() + ",D0");
+
+        }
+
+    }
+
+    public void multiply() {
+        matchChar('*');
+        factor();
+        writeToOutput("MULS (SP)+,D0");
+
+    }
+
+    public void divide() {
+        matchChar('/');
+        factor();
+        // why not just do
+        // because of the order that division happens and where it would be stored
+        // writeToOutput("DIVS (SP)+,D0");
+
+        writeToOutput("MOVE (SP)+, D1");
+        writeToOutput("DIVS D0,D1");
+        writeToOutput("MOVE D1,D0");
+
+    }
+
+    // <term> ::= <factor> [ <mulop> <factor ]*
+    // note how a term relates to an expression
+    // For example 3 + 2 * 2 * 2 + 5 = 3 + (2 * 2 * 2) + 3 = <term> + <term> +
+    // <term>
+    // multiplication and division are "integrated in" and arent added to the
+    // expression operations
     public void term() {
-        writeToOutput("MOVE# " + getNumber() + ",D0");
+
+
+            factor();
+
+            while (look == '*' || look == '/') {
+
+                writeToOutput("MOVE D0,-(SP)");
+                switch (look) {
+                case '*':
+                    multiply();
+                    break;
+                case '/':
+                    divide();
+                    break;
+                default:
+                    writeExpected("Expected MulOp");
+                }
+
+            }
+
+        
+
     }
 
     public static void main(String[] args) throws Exception {
